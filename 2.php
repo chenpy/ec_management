c51cc95e3ecf3cd961c1a47765f8c583bed2256a
chenpy <poyu0603@gmail.com>
2016-09-08 19:58:51 +0900

    Add upload csv logic of coupon site(except groupon)
    *Created table schema of coupon site(except groupon)
     Please enter the commit message for your changes. Lines starting

--- 2.php
+++ 2.php
@@ -14,31 +14,31 @@
 <body>
   <h1>クーポンサイト出荷処理</h1>
 	<div class="upload">
-  		<form action="upload.php" method="post" enctype="multipart/form-data">
+  		<form action="2_file_upload.php" method="post" enctype="multipart/form-data">
     		ラクーポン
-    	<input type="file" name="fileToUpload">
-   	 	<input type="submit" value="Upload" name="submit">
+    	<input type="file" name="racoupon">
+   	 	<input type="submit" value="Upload" name="racouponUpload">
 		</form>
   	</div>
   	<div class="upload">
-  		<form action="upload.php" method="post" enctype="multipart/form-data">
+  		<form action="2_file_upload.php" method="post" enctype="multipart/form-data">
     		グルーポン
-    	<input type="file" name="fileToUpload">
-   	 	<input type="submit" value="Upload" name="submit">
+    	<input type="file" name="groupon">
+   	 	<input type="submit" value="Upload" name="grouponUpload">
 		</form>
   	</div>
   	<div class="upload">
-  		<form action="upload.php" method="post" enctype="multipart/form-data">
+  		<form action="2_file_upload.php" method="post" enctype="multipart/form-data">
     		ポンパレ
-    	<input type="file" name="fileToUpload">
-   	 	<input type="submit" value="Upload" name="submit">
+    	<input type="file" name="ponpare">
+   	 	<input type="submit" value="Upload" name="ponpareUpload">
 		</form>
   	</div>
   	<div class="upload">
-  		<form action="upload.php" method="post" enctype="multipart/form-data">
+  		<form action="2_file_upload.php" method="post" enctype="multipart/form-data">
     		サンプル百貨店
-    	<input type="file" name="fileToUpload">
-   	 	<input type="submit" value="Upload" name="submit">
+    	<input type="file" name="3ple">
+   	 	<input type="submit" value="Upload" name="3pleUpload">
 		</form>
   	</div>
   	<div class="upload">
---
+++ 2_file_upload.php
@@ -0,0 +1,122 @@
+<?php
+// Connect to database
+include 'mysql_connect.php';
+
+function delete_old_data($tableName,$conn){
+  $delete_old_sql="DELETE FROM $tableName WHERE DATE(`upload`) = CURDATE();";
+  if ($conn->query($delete_old_sql) === TRUE) {
+      echo "Delete old data of $tableName successfully<br>";
+  } else {
+      echo "Error delete old data: " . $conn->error."<br>";
+  }
+}
+function delete_summary_old_data($mall,$conn){
+  $delete_old_sql="DELETE FROM summary WHERE `出荷日` = CURDATE() AND `モール`='$mall';";
+  if ($conn->query($delete_old_sql) === TRUE) {
+      echo "Delete old data of summary successfully<br>";
+  } else {
+      echo "Error delete old data: " . $conn->error."<br>";
+  }
+}
+
+// Racoupon start
+if(isset($_POST["racouponUpload"]) && $_FILES["racoupon"]["error"] == UPLOAD_ERR_OK ){  
+    // Yahoo items information handling
+    $tmp_name = $_FILES["racoupon"]["tmp_name"];
+        // basename() may prevent filesystem traversal attacks;
+        // further validation/sanitation of the filename may be appropriate
+    $name = basename($_FILES["racoupon"]["name"]);
+    move_uploaded_file($tmp_name, "uploads/$name");
+    delete_old_data('racoupon_orig',$conn);
+    $sql = "LOAD DATA LOCAL INFILE "."'/Library/WebServer/Documents/uploads/".$_FILES[racoupon][name]."'
+    INTO TABLE racoupon_orig
+    FIELDS TERMINATED BY ',' 
+    ENCLOSED BY '\"'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 ROWS";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Insert data successfully<br>";
+    } else {
+        echo "Error Insert table: " . $conn->error;
+    }
+}
+// Racoupon END
+
+// // groupon start 
+
+// if(isset($_POST["grouponUpload"]) && $_FILES["groupon"]["error"] == UPLOAD_ERR_OK ){
+//     // groupon items information handling
+//     $tmp_name = $_FILES["groupon"]["tmp_name"];
+//     // basename() may prevent filesystem traversal attacks;
+//     // further validation/sanitation of the filename may be appropriate
+//     // TODO handle *.xls format
+//     $name = basename($_FILES["groupon"]["name"]);
+//     move_uploaded_file($tmp_name, "uploads/$name");
+//     delete_old_data('groupon_original',$conn);
+//     $sql = "LOAD DATA LOCAL INFILE "."'/Library/WebServer/Documents/uploads/".$_FILES[ponpare][name]."'
+//     INTO TABLE groupon_orig
+//     FIELDS TERMINATED BY ',' 
+//     ENCLOSED BY '\"'
+//     LINES TERMINATED BY '\r\n'
+//     IGNORE 1 ROWS";
+
+//     if ($conn->query($sql) === TRUE) {
+//         echo "Insert groupon data successfully<br>";
+//     } else {
+//         echo "Error Insert table: " . $conn->error;
+//     }
+// }
+// // Groupon END
+
+//Ponpare start
+
+if(isset($_POST["ponpareUpload"]) && $_FILES["ponpare"]["error"] == UPLOAD_ERR_OK ){
+    // Yahoo items information handling
+    $tmp_name = $_FILES["ponpare"]["tmp_name"];
+        // basename() may prevent filesystem traversal attacks;
+        // further validation/sanitation of the filename may be appropriate
+    $name = basename($_FILES["ponpare"]["name"]);
+    move_uploaded_file($tmp_name, "uploads/$name");
+    delete_old_data('ponparetic_orig',$conn);
+    $sql = "LOAD DATA LOCAL INFILE '/Library/WebServer/Documents/uploads/".$_FILES[ponpare][name]."'
+  INTO TABLE ponparetic_orig 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 ROWS";
+
+  if ($conn->query($sql) === TRUE) {
+        echo "Insert data successfully<br>";
+    } else {
+        echo "Error Insert table: " . $conn->error;
+    }
+}
+
+//ponpare end
+
+//3ple start 
+if(isset($_POST["3pleUpload"]) && $_FILES["3ple"]["error"] == UPLOAD_ERR_OK ){  
+
+    // Yahoo items information handling
+    $tmp_name = $_FILES["3ple"]["tmp_name"];
+        // basename() may prevent filesystem traversal attacks;
+        // further validation/sanitation of the filename may be appropriate
+    $name = basename($_FILES["3ple"]["name"]);
+    move_uploaded_file($tmp_name, "uploads/$name");
+    delete_old_data('3ple_orig',$conn);
+    $sql = "LOAD DATA LOCAL INFILE "."'/Library/WebServer/Documents/uploads/".$_FILES['3ple'][name]."'
+    INTO TABLE 3ple_orig
+    FIELDS TERMINATED BY ',' 
+    ENCLOSED BY '\"'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 ROWS";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Insert 3ple data successfully<br>";
+    } else {
+        echo "Error Insert table: " . $conn->error;
+    }
+  }
+//3ple end
+?>
